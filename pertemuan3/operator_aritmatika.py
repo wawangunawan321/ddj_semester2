978e79b0d2b07468e561658d7ca18243f36dc681
wawangunawan321 <157335466+wawangunawan321@users.noreply.github.com>
2024-01-29 09:32:20 +0700

    Add files via upload

---
+++ pertemuan3/operator_aritmatika.py
@@ -0,0 +1,10 @@
+x = 5
+y = 3
+
+print("penjumlahan 5 + 3 = ", x+y)
+print("pengurangan 5 - 3 = ", x-y)
+print("perkalian 5 * 3 = ", x*y)
+print("pembagian 5 / 3 = ", x/y)
+print("pwmbagian bulat 5 // 3 = ", x//y)
+print("modulus 5 % 3 = ", x%y)
+print("perpangkatan 5 ** 3 = ", x**y)
